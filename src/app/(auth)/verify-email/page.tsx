5f0f257a5e6431e8dfd1141d1b4075712a800daf
PavanVarma474 <pavanvarma474@gmail.com>
2025-08-20 15:22:29 +0530

    Frontend deploy

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,6 +1,10 @@
 "use client";
+
 import React, { Suspense } from "react";
 import VerifyEmail from "./verify-email-content";
+
+// Prevent static export issues
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return (
